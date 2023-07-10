511197eae313a9548c44e5f03399f6dc85397f5c
Ruslan Mamedbekov <m4medbekov@gmail.com>
2023-07-10 17:49:30 +0300

    Add react

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,11 @@ module.exports = {
   output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
   mode: process.env.NODE_ENV || "development",
   resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-  devServer: { contentBase: path.join(__dirname, "src") },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
