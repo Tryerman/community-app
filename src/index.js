a1bc5b85dc17bcce805735f9dd7bad721491bb0c
Ruslan Mamedbekov <m4medbekov@gmail.com>
2023-07-12 12:46:41 +0300

    Update ReactDOM.render to createRoot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-const HelloWorld = () => {
-  return <h1>Hello World</h1>;
-};
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+root.render(<App />);
