eba58718069b502332ede88d88b9a20d4039dae9
Ruslan Mamedbekov <m4medbekov@gmail.com>
2023-07-12 12:32:42 +0300

    Fix error with node-sass + add babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,26 @@ module.exports = {
       directory: path.resolve(__dirname, "src"),
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, "public", "index.html"),
     }),
   ],
 };
